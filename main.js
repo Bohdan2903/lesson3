df0a61805aceafd84be7bf2c8913359ac0b9cab9
LutsenkoDmitro <dmitriylutsenkoo@gmail.com>
2022-12-22 22:41:37 +0200

    create page

--- main.js
+++
@@ -1,4 +0,0 @@
-function completeAndRedirect() {
-  var form = document.forms['myform'];
-  text.innerHTML = "Веденні вами дані: <br> І'мя : " + form.elements.Name.value + "<br> EMAIL : " + form.elements.login.value + "<br> ПОвідомлення : " + form.elements.message.value;
-}
